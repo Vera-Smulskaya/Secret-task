d4fe0fd834a2c4fa6d3e0701bf088decbd7b1570
Anhelina Lebedieva <a.lebedieva@godeltech.com>
2023-01-24 13:22:35 +0100

    Become a billionaire

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function main() {
     refs = {
         button: document.querySelector(".display__btn"),
-        song: document.querySelector(".song")
+        // song: document.querySelector(".song"),
+        video: document.querySelector("video")
     }
 
     const goGoGoParty = () => {
         console.log("click")
-        refs.song.play();
-        refs.song.classList.add("party-started");
+        // refs.song.play();
+        // refs.song.classList.add("party-started");
+        refs.video.play();
+        refs.video.classList.add("party-started");
+        refs.button.style.display= "none";
     }
 
 
